e5c1e5ce13cb5bc8e7758cfe38e8eb09f3717e42
zeeshanshaikh <skzeeshan10011@gmail.com>
2025-07-28 16:28:05 +0530

    githooks test 1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,17 @@
 // @ts-check
-
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.recommendedTypeChecked,
-    {
-        ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
-        languageOptions: {
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
-        rules: {},
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        project: ['./tsconfig.json'],
+      },
     },
+  }
 );
