0b038a61c0111f2bcdf9c6905c7586d3d96591b2
zeeshanshaikh <skzeeshan10011@gmail.com>
2025-08-27 22:25:51 +0530

    setup test coverage

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,15 @@ module.exports = {
     ...createDefaultPreset(),
     testEnvironment: 'node',
     verbose: true,
+    collectCoverage: true,
+    coverageProvider: 'v8', // faster than babel for TS/JS
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}', // all ts/tsx files under src
+        '!src/**/*.d.ts', // ignore type definitions
+        '!src/**/index.ts', // ignore barrel files
+        '!tests/**', // ignore test files
+        '!**/node_modules/**', // always ignore node_modules
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov', 'json', 'html'], // multiple output formats
 };
